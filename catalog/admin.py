c65eb860679cbb4274ed09f9ae47510e1f50c1bb
ErDmKo <erdmko@gmail.com>
2024-05-11 12:44:36 +0200

    Searcheble name and decimals to store amount of items

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import CatalogItem 
 
-admin.site.register(CatalogItem)
+@admin.register(CatalogItem)
+class CatalodItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'count']
+    list_editable = ['count']
+    list_filter = ['count']
+    search_fields = ['name__icontains']
+
--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class CatalogItem(models.Model):
     name = models.CharField(max_length=200)
-    count = models.IntegerField(default=0)
+    count = models.DecimalField(
+        default=0,
+        max_digits=100,
+        decimal_places=5
+    )
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
