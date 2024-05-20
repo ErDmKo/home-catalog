b2776995f19111458a1ca14ec65ee4b4a6495d00
ErDmKo <erdmko@gmail.com>
2024-05-20 10:50:06 +0200

    New fields grops and to_buy search and display

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
-from .models import CatalogItem
+from .models import CatalogItem, ItemGroup
 
+@admin.register(ItemGroup)
+class ItemGroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.display
+def catalog_item_name(obj):
+    return str(obj)
 
 @admin.register(CatalogItem)
-class CatalodItemAdmin(admin.ModelAdmin):
-    list_display = ["name", "count"]
-    list_editable = ["count"]
-    list_filter = ["count"]
-    search_fields = ["name__icontains"]
+class CatalogItemAdmin(admin.ModelAdmin):
+    list_display = [catalog_item_name, "to_buy"]
+    list_editable = ["to_buy"]
+    list_filter = ["to_buy"]
+    search_fields = ["name__icontains", "group__title"]
     ordering = ["name", "pk"]
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,16 +1,30 @@
 from django.db import models
+from datetime import datetime
 
+class ItemGroup(models.Model):
+    title = models.CharField("Название группы", max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name_plural = "Группы вещей"
 
 class CatalogItem(models.Model):
     name = models.CharField("Наименование вещи", unique=True, max_length=200)
     count = models.DecimalField(
         "Количество", default=0, max_digits=100, decimal_places=5
     )
-    pub_date = models.DateTimeField("Дата публикации")
-    ordering = ["name"]
+    pub_date = models.DateTimeField("Дата публикации", default=datetime.now)
+    to_buy = models.BooleanField("Нужно купить", default=False)
+    group = models.ManyToManyField(ItemGroup, blank=True)
 
     class Meta:
+        ordering = ["name"]
         indexes = [models.Index(fields=["name"], name="name_idx")]
+        verbose_name_plural = "Каталог вещей"
 
     def __str__(self):
-        return f"{self.name} ({self.count})"
+        grops = "".join([f"[{group.title}]" for group in self.group.all()])
+        return f"{grops} {self.name} ({self.count:.2f})"
