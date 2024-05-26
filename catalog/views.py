0a884121a1f94e0aef1648fac1f971d291b6dfe0
ErDmKo <erdmko@gmail.com>
2024-05-26 16:13:42 +0200

    Open catagories

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import CatalogItem, ItemGroup
 from django.db.models import Q
 
-query_parmas = ["only_to_by", "group"]
+query_parmas = ["only_to_by", "group", "flat_view", "error"]
+
+
+def encode(str_query):
+    return urlencode(str_query, quote_via=quote)
 
 
 def get_query_state(request):
@@ -19,9 +23,10 @@ def get_query_state(request):
 
 def update(request, catalog_item_id):
     path = reverse("index")
-    query = urlencode(get_query_state(request))
+    query = encode(get_query_state(request))
     if not request.user.is_authenticated:
-        return redirect(f"{path}?{query}")
+        error_query = encode({"error": "not authenticated"})
+        return redirect(f"{path}?{error_query}")
     item = CatalogItem.objects.get(id=catalog_item_id)
     item.to_buy = not item.to_buy
     item.save()
@@ -40,14 +45,19 @@ def index(request):
         groups = []
         selected_group = ItemGroup.objects.get(id=query_dict["group"])
         list_query = list_query & Q(group=query_dict["group"])
+    elif query_dict.get("flat_view"):
+        groups = []
+        list_query = list_query
     else:
         list_query = list_query & Q(group=None)
+    str_query = encode(query_dict)
     return render(
         request,
         "catalog/index.html",
         {
             "query_dict": query_dict,
-            "query": urlencode(query_dict),
+            "query": str_query,
+            "fileds_to_safe": query_parmas,
             "groups": groups,
             "selected_group": selected_group,
             "latest_catalog_list": CatalogItem.objects.filter(list_query),
