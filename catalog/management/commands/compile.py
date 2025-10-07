925a4f06da4d43b322275144e2924624fe26ec84
Dmitrii Eroshenko <dmitrii.eroshenko@zalando.de>
2025-10-07 22:37:16 +0200

    Compile sctipt fix

--- catalog/management/commands/compile.py
+++ catalog/management/commands/compile.py
@@ -1,15 +1,27 @@
-from django.core.management.base import BaseCommand
+from pathlib import Path
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
 from esbuild_py import build
+
+BASE_DIR = Path(settings.BASE_DIR)
+ENTRY_POINT = BASE_DIR / "frontend" / "index.ts"
+OUTFILE = BASE_DIR / "catalog" / "static" / "catalog" / "js" / "bundle.js"
 
 
 class Command(BaseCommand):
-    help = "Compile typesctiprt app to static"
+    help = "Compile typescript app to static"
 
     def compile_cmd(self):
-        build(
-            entry_points=["./frontend/index.ts"],
-            outfile="./catalog/static/catalog/js/bundle.js",
+        compile_result = build(
+            entry_points=[str(ENTRY_POINT)],
+            outfile=str(OUTFILE),
         )
+        if compile_result.get("errors"):
+            self.stdout.write(self.style.ERROR("Compile error:"))
+            for error in compile_result["errors"]:
+                self.stdout.write(f'- {error["text"]}')
+            raise CommandError("TypeScript compilation failed.")
+
 
     def handle(self, *args, **options):
         self.compile_cmd()
